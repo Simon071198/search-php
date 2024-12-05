7ca81945755c632d698b172ce1d2b2fbcdba92af
Simon071198 <81679661+Simon071198@users.noreply.github.com>
2024-12-05 17:17:47 +0700

    menambahkan halaman search

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman Form Pencarian</title>
+</head>
+<body>
+  <div>
+    <!-- halaman form pencarian -->
+     <form action="hasil-cari.php" method="post">
+      <input type="text" name="search" id="">
+     </form>
+  </div>
+</body>
+</html>
